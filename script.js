22f06c415aa0933751eab6c785d45a301885aba1
Md Saiful Islam <mdsaifulislamrafsan099@gmail.com>
2024-10-06 03:16:36 +0600

    added getDish data function

--- script.js
+++ script.js
@@ -2,4 +2,13 @@ const searchForm = document.getElementById("searchForm");
 searchForm.addEventListener('submit', (e) => {
         e.preventDefault();  
     });
-    
+
+    async function getDishData(){
+        const res = await fetch('./dish.json')
+        const data = await res.json();
+        console.log(data)
+
+
+    }
+    getDishData()
+
