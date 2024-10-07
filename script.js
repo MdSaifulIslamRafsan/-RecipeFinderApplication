1f9a9d82ede6240711b191f5e326e68a0a6601c1
Md Saiful Islam <mdsaifulislamrafsan099@gmail.com>
2024-10-07 17:38:34 +0600

    added dishcard

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const searchForm = document.getElementById("searchForm"); 
+const dishContainer = document.getElementById("dish-container")
 searchForm.addEventListener('submit', (e) => {
         e.preventDefault();  
     });
@@ -6,9 +7,25 @@ searchForm.addEventListener('submit', (e) => {
     async function getDishData(){
         const res = await fetch('./dish.json')
         const data = await res.json();
-        console.log(data)
+        showDishCard(data)
 
 
     }
     getDishData()
 
+function showDishCard(dishData){
+    dishData.forEach(data => {
+        const div = document.createElement('div');
+        div.classList.add('card')
+        console.log(data);
+        div.innerHTML = `
+        <img src=${data?.recipeImage} alt=${data?.dishName}/>
+        <h2>${data?.dishName}</h2>
+        <p>${data?.shortDescription}</p>
+        <p>Cooking Time : ${data?.cookingTime}</p>
+        <p>Preparation Time : ${data?.preparationTime}</p>
+        `
+        dishContainer.appendChild(div)
+        
+    });
+}
